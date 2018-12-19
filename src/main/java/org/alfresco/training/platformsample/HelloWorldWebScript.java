b554aaeacd03719a5a60dc1c8a2d69439b455d8f
Martin Bergljung <gravitonian@gmail.com>
2018-12-19 13:22:38 +0000

    first commit

---
+++ src/main/java/org/alfresco/training/platformsample/HelloWorldWebScript.java
@@ -0,0 +1,58 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package org.alfresco.training.platformsample;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.springframework.extensions.webscripts.Cache;
+import org.springframework.extensions.webscripts.DeclarativeWebScript;
+import org.springframework.extensions.webscripts.Status;
+import org.springframework.extensions.webscripts.WebScriptRequest;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A demonstration Java controller for the Hello World Web Script.
+ *
+ * @author martin.bergljung@alfresco.com
+ * @since 2.1.0
+ */
+public class HelloWorldWebScript extends DeclarativeWebScript {
+    private static Log logger = LogFactory.getLog(HelloWorldWebScript.class);
+
+    protected Map<String, Object> executeImpl(
+            WebScriptRequest req, Status status, Cache cache) {
+        Map<String, Object> model = new HashMap<String, Object>();
+
+        Connection.Response html = null;
+        try {
+            html = Jsoup.connect("http://www.example.com/").execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        model.put("fromJava", "HelloFromJava(" + html.body() + ")");
+
+        logger.debug("Your 'Hello World' Web Script was called!");
+
+        return model;
+    }
+}
